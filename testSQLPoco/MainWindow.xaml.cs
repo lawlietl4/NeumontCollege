db39a63107c9120a9a2d233b0e19d3efba7a4ecb
Nicholas Roberson <60959101+lawlietl4@users.noreply.github.com>
2021-05-13 09:27:23 -0600

    Add files via upload

---
+++ testSQLPoco/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using testSQLPoco.Data;
+
+namespace testSQLPoco
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly NeumontContext _context = new NeumontContext();
+        private CollectionViewSource studentsViewSource; 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //checks if there are new things in the migration folder to be added to the DB
+            _context.Database.Migrate();
+
+            _context.Students.Load();
+            studentsViewSource.Source = _context.Students.Local.ToObservableCollection();
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            _context.Dispose();
+            base.OnClosing(e);
+        }
+        protected void Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            _context.SaveChanges();
+            studentDataGrid.Items.Refresh();
+        }
+    }
+}
---
+++ testSQLPoco/Migrations/20210505234438_initialMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace testSQLPoco.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    gpa = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
