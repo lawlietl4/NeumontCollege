cd08c0ce533a2b2ecfde295fc8a60079264c045e
Nicholas Roberson <60959101+lawlietl4@users.noreply.github.com>
2021-05-13 09:23:14 -0600

    Add files via upload

---
+++ MatchGame/Controls/Card.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MatchGame.Controls
+{
+    public partial class Card : UserControl
+    {
+        public Card()
+        {
+            InitializeComponent();
+            Loaded += Card_Loaded;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public enum eState { Inactive, Idle, Flipped, Matched }
+        public GameWindow Owner;
+        private eState state { get; set; } = eState.Inactive;
+        public eState State
+        {
+            get { return state; }
+            set
+            {
+                if (value != state)
+                {
+                    state = value;
+                    Interactable = (state == eState.Idle);
+                    Show = (state == eState.Flipped || state == eState.Matched);
+                    NotifyPropertyChanged(nameof(State));
+                }
+            }
+        }
+
+        public bool Show
+        {
+            set
+            {
+                if (value)
+                {
+                    lblSymbol.Visibility = Visibility.Visible;
+                    imgCard.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    lblSymbol.Visibility = Visibility.Hidden;
+                    imgCard.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        private bool interactable;
+        public bool Interactable
+        {
+            get { return interactable; }
+            set
+            {
+                if (value != interactable)
+                {
+                    interactable = value;
+                    NotifyPropertyChanged(nameof(Interactable));
+                }
+            }
+        }
+
+        private string symbol;
+
+        public string Symbol
+        {
+            get { return symbol; }
+            set
+            {
+                if (value != symbol)
+                {
+                    symbol = value;
+                    NotifyPropertyChanged("Symbol");
+                }
+            }
+        }
+
+        private void Card_Loaded(object sender, RoutedEventArgs e)
+        {
+            Owner = (GameWindow)Window.GetWindow(btnCard);
+            Owner.RegisterCard(this);
+        }
+
+        private void btnCard_Click(object sender, RoutedEventArgs e)
+        {
+            State = eState.Flipped;
+            Owner.SelectCard(this);
+        }
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MatchGame/GameWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using MatchGame.Controls;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace MatchGame
+{
+    public partial class GameWindow : Window
+    {
+        private Random random = new Random();
+        private DispatcherTimer timer = new DispatcherTimer();
+        List<Card> cards = new List<Card>();
+        List<string> symbols = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",",
+            "b", "b", "v", "v", "w", "w",
+        };
+        private Card card1 = null;
+        private Card card2 = null;
+        public GameWindow()
+        {
+            timer.Interval = TimeSpan.FromSeconds(1.0);
+            timer.Tick += TimerTick;
+        }
+        public void RegisterCard(Card card)
+        {
+            card.State = Card.eState.Idle;
+            int r = random.Next(symbols.Count);
+            card.Symbol = symbols[r];
+            symbols.RemoveAt(r);
+            cards.Add(card);
+        }
+        public void SelectCard(Card card)
+        {
+            int correctNum = 5;
+            if (card1 == null)
+            {
+                //set card1 to card
+                card1 = card;
+            }
+            else
+            {
+                //set card2 to card
+                card2 = card;
+                if (card1.Symbol == card2.Symbol)
+                {
+                    //set card1 and card2 to matched
+                    card1.State = Card.eState.Matched;
+                    card2.State = Card.eState.Matched;
+                    //set card1 and card2 to null
+                    card1 = null;
+                    card2 = null;
+                }
+                else
+                {
+                    correctNum -= 1;
+                    //disable all cards, so they can't be selected for a second
+                    //this allows the player to see the flipped card for a moment
+                    foreach (Card c in cards)
+                    {
+                        if (c.State == Card.eState.Idle)
+                        {
+                            //set card to inactive
+                            card.State = Card.eState.Inactive;
+                        }
+                    }
+                    //start timer, after a second the TimerTick is called and the cards reset
+                    //timer.Start();
+                    timer.Interval -= TimeSpan.FromSeconds(1);
+                    //timer.Stop();
+                }
+            }
+        }
+        private void TimerTick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            foreach (Card c in cards)
+            {
+                if (!(c.State == Card.eState.Matched))
+                {
+                    c.State = Card.eState.Idle;
+                }
+                card1 = null;
+                card2 = null;
+            }
+        }
+
+        //internal void SelectCard(Card card)
+        //{
+            
+        //}
+    }
+}
